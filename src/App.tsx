5ba5236f7d8b56843745edbb4742e5da0db6cd45
parssak <parssak@gmail.com>
2022-08-20 08:39:06 -0400

    feat: working template

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import "./index.css";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
+    <div className='container mx-auto py-6'>
+      <h1 className="font-bold text-3xl">Starter Template</h1>
       <p>Vite, React, TypeScript, TailwindCSS 3</p>
     </div>
   );
