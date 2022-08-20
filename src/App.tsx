ed0602430ab11739cc856e6742398227cb6b28a5
parssak <parssak@gmail.com>
2022-08-20 08:34:29 -0400

    feat: :rocket: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
+      <p>Vite, React, TypeScript, TailwindCSS 3</p>
+    </div>
+  );
+}
+
+export default App;
